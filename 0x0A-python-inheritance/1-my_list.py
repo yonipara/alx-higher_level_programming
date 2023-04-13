0847164de3b9dd6f01641f8f83273ad320bf17db
yonipara <yonisolo12@gmail.com>
2023-04-13 18:19:19 +0300

    python inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+""" 0x0A. Python - Inheritance, task 1 """
+
+
 class MyList(list):
+    """Custom list type intended to only contain integers.
+    """
+
     def print_sorted(self):
-        print (sorted(self))
+        """Prints MyList lists in ascending order by value.
+        """
+        sorted_list = self[:]
+        sorted_list.sort()
+        print(sorted_list)

cbd82505e60b7fb501f117222be0da4db443c0b2
yonipara <yonisolo12@gmail.com>
2023-04-13 18:33:21 +0300

    python inheritance

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+""" 0x0A. Python - Inheritance, task 6 """
+
+
+class BaseGeometry:
+    """Intentionally empty area() method.
+    """
+    def area(self):
+        """Unimplemented, only raises exception to notify user.
+        """
+        raise Exception('area() is not implemented')

ed1213b16c5f0695ca025604e7cc532b8d1229a5
yonipara <yonisolo12@gmail.com>
2023-04-19 12:50:09 +0300

    python more classes

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,90 +1,98 @@
-# Python3 program to solve N Queen
-# Problem using backtracking
-global N
-N = 4
+class QueenChessBoard:
+    def __init__(self, size):
+        # board has dimensions size x size
+        self.size = size
+        # columns[r] is a number c if a queen is placed at row r and column c.
+        # columns[r] is out of range if no queen is place in row r.
+        # Thus after all queens are placed, they will be at positions
+        # (columns[0], 0), (columns[1], 1), ... (columns[size - 1], size - 1)
+        self.columns = []
  
-def printSolution(board):
-    for i in range(N):
-        for j in range(N):
-            print(board[i][j], end = " ")
-        print()
+    def place_in_next_row(self, column):
+        self.columns.append(column)
  
-# A utility function to check if a queen can
-# be placed on board[row][col]. Note that this
-# function is called when "col" queens are
-# already placed in columns from 0 to col -1.
-# So we need to check only left side for
-# attacking queens
-def isSafe(board, row, col):
+    def remove_in_current_row(self):
+        return self.columns.pop()
  
-    # Check this row on left side
-    for i in range(col):
-        if board[row][i] == 1:
-            return False
+    def is_this_column_safe_in_next_row(self, column):
+        # index of next row
+        row = len(self.columns)
  
-    # Check upper diagonal on left side
-    for i, j in zip(range(row, -1, -1),
-                    range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
- 
-    # Check lower diagonal on left side
-    for i, j in zip(range(row, N, 1),
-                    range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
- 
-    return True
- 
-def solveNQUtil(board, col):
-     
-    # base case: If all queens are placed
-    # then return true
-    if col >= N:
+        # check column
+        for queen_column in self.columns:
+            if column == queen_column:
+                return False
+        # check diagonal
+        for queen_row, queen_column in enumerate(self.columns):
+            if queen_column - queen_row == column - row:
+                return False
+
+        # check other diagonal
+        for queen_row, queen_column in enumerate(self.columns):
+            if ((self.size - queen_column) - queen_row
+                == (self.size - column) - row):
+                return False
+
         return True
- 
-    # Consider this column and try placing
-    # this queen in all rows one by one
-    for i in range(N):
- 
-        if isSafe(board, i, col):
-             
-            # Place this queen in board[i][col]
-            board[i][col] = 1
- 
-            # recur to place rest of the queens
-            if solveNQUtil(board, col + 1) == True:
-                return True
- 
-            # If placing queen in board[i][col
-            # doesn't lead to a solution, then
-            # queen from board[i][col]
-            board[i][col] = 0
- 
-    # if the queen can not be placed in any row in
-    # this column col then return false
-    return False
- 
-# This function solves the N Queen problem using
-# Backtracking. It mainly uses solveNQUtil() to
-# solve the problem. It returns false if queens
-# cannot be placed, otherwise return true and
-# placement of queens in the form of 1s.
-# note that there may be more than one
-# solutions, this function prints one of the
-# feasible solutions.
-def solveNQ():
-    board = [ [0, 0, 0, 0],
-              [0, 0, 0, 0],
-              [0, 0, 0, 0],
-              [0, 0, 0, 0] ]
- 
-    if solveNQUtil(board, 0) == False:
-        print ("Solution does not exist")
-        return False
- 
-    printSolution(board)
-    return True
- 
-# Driver Code
-solveNQ()
+
+    def display(self):
+        for row in range(self.size):
+            for column in range(self.size):
+                if column == self.columns[row]:
+                    print('Q', end=' ')
+                else:
+                    print('.', end=' ')
+            print()
+    def solve_queen(size):
+    """Display a chessboard for each possible configuration of placing n queens
+    on an n x n chessboard and print the number of such configurations."""
+    board = QueenChessBoard(size)
+    number_of_solutions = 0
+
+    row = 0
+    column = 0
+    # iterate over rows of board
+    while True:
+        # place queen in next row
+        while column < size:
+            if board.is_this_column_safe_in_next_row(column):
+                board.place_in_next_row(column)
+                row += 1
+                column = 0
+                break
+            else:
+                column += 1
+    # if could not find column to place in or if board is full
+        if (column == size or row == size):
+            # if board is full, we have a solution
+            if row == size:
+                board.display()
+                print()
+                number_of_solutions += 1
+
+                # small optimization:
+                # In a board that already has queens placed in all rows except
+                # the last, we know there can only be at most one position in
+                # the last row where a queen can be placed. In this case, there
+                # is a valid position in the last row. Thus we can backtrack two
+                # times to reach the second last row.
+                board.remove_in_current_row()
+                row -= 1
+
+            # now backtrack
+            try:
+                prev_column = board.remove_in_current_row()
+            except IndexError:
+                # all queens removed
+                # thus no more possible configurations
+                break
+            # try previous row again
+            row -= 1
+            # start checking at column = (1 + value of column in previous row)
+            column = 1 + prev_column
+
+    print('Number of solutions:', number_of_solutions)
+
+
+n = int(input('Enter n: '))
+solve_queen(n)

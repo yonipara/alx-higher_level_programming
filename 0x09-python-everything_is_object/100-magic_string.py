c8ed16037ff4eabb94b42cd72fa9ef3a74748b5b
yonipara <yonisolo12@gmail.com>
2023-04-19 17:20:35 +0300

    python objects

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,17 +1,4 @@
 #!/usr/bin/python3
-
 def magic_string():
-    # function objects can have attributes, here at global count of calls
-    if hasattr(magic_string, 'calls'):
-        magic_string.calls += 1
-    else:
-        magic_string.calls = 1
-
-    return ', '.join(['BestSchool'] * magic_string.calls)
-
-# Original solution
-#
-# #!/usr/bin/python3
-# def magic_string():
-#     setattr(magic_string, 'rep', getattr(magic_string, 'rep', -1) + 1)
-#     return 'Holberton' + ', Holberton' * getattr(magic_string, 'rep', 0)
+    magic_string.n = getattr(magic_string, 'n', 0) + 1
+    return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")

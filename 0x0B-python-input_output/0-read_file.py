3479a0540532637aefca64c2cf1f0ad2b57c2119
yonipara <yonisolo12@gmail.com>
2023-04-19 17:33:48 +0300

    python i/o

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+"""0x0B. Python - Input/Output, task 0. Read file """
+
+
 def read_file(filename=""):
-    with open(filename, encoding="utf-8") as f:
-        lineNum = 1
-        while(True):
-            line = f.readline()
-            if not line:
-                break
-            print(line, end="")
-            lineNum += 1
+    """Reads contents of a text file and print to stdout.
+    Args:
+        filename (str): name of file to be opened
+    """
+    with open(filename, encoding='utf-8') as file:
+        print(file.read(), end='')

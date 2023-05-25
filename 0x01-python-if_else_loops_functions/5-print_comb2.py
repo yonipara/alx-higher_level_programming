fb16b3aadf3beaa03cf6d4f6e46be4a33cf3328b
yonipara <yonisolo12@gmail.com>
2023-05-25 17:19:42 +0300

    python if elif else

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-for number in range(0, 100):
-    if number == 99:
-        print("{}".format(number))
-    else:
-        print("{:02}".format(number), end=", ")
+for i in range(0, 10):
+    for j in range(0, 10):
+        if i == 9 and j == 9:
+            break
+        print("{}{}, ".format(i, j), end="")
+
+print("{}{}".format(i, j))

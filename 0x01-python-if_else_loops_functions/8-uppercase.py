9e3d460a2638fd2e2384ae97050f965439434336
yonipara <yonisolo12@gmail.com>
2023-05-26 18:22:55 +0300

    python if elif else

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -6,8 +6,6 @@ def uppercase(str):
     """Print a string in uppercase."""
     for char in str:
         if ord(char) >= 97 and ord(char) <= 122:
-            char = ord(char) - 32
-            print("{}".format(chr(char)), end="")
-        else:
-            print("{}".format(char), end="")
+            char = chr(ord(char) - 32)
+        print("{}".format(char), end="")
     print()

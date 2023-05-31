fa019f8c82776743be6fb05f2013beb3455effbe
yonipara <yonisolo12@gmail.com>
2023-05-31 14:28:15 +0300

    python if elif else

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -6,7 +6,7 @@ x = 122
 for i in range(1, 27):
     if x % 2 == 0:
         print("{}".format(chr(x)), end="")
-    if x % 2 != 0:
+    else x % 2 != 0:
         y = x - 32
         print("{}".format(chr(y)), end="")
     x -= 1

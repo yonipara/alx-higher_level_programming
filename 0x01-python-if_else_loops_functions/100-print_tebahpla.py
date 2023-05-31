082311966085d60440a84a210cc1120ab17b09ca
yonipara <yonisolo12@gmail.com>
2023-05-31 14:20:59 +0300

    python if elif else

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,7 +2,11 @@
 # 100-print_tebahpla.py
 
 """"Print the alphabet in reverse order alternating upper- and lower-case."""
-i = 0
-for c in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(c - i)), end="")
-    i = 32 if i == 0 else 0
+x = 122
+for i in range(1, 27):
+    if x % 2 == 0:
+        print("{}".format(chr(x)), end="")
+    if x % 2 != 0:
+        y = x - 32
+        print("{}".format(chr(y)), end="")
+    x -= 1

b2e11efd5d644f611b8c2625a54321822da11348
yonipara <65731614+yonipara@users.noreply.github.com>
2024-02-09 21:00:35 +0300

    data structures in python

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -15,4 +15,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
         else:
             tuple_b = tuple_b[0], 0
 
-    return tuple(map(lambda x, y: x + y, tuple_a, tuple_b))
+    return tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]

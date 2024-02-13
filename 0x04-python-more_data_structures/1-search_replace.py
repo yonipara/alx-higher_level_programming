96cdd684397e93a922eb12119d98bc4937c46ae2
yonipara <yonisolo12@gmail.com>
2024-02-13 22:32:30 +0300

    more data structures

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    """ replaces an element with a new one """
     new_list = list(map(lambda x: replace if x == search else x, my_list))
     return new_list
-

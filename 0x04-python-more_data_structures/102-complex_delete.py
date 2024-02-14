81120c1905642687a7d514aef87520737784805c
yonipara <yonisolo12@gmail.com>
2024-02-14 22:03:44 +0300

    more data structures

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,6 @@
-
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
+    """ function that deletes keys with a specific value in a dictionary """
     list_keys = list(a_dictionary.keys())
 
     for value_dic in list_keys:

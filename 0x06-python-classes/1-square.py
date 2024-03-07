bb1429a6dd7627edd096b6b8788cd62b77e2fb36
yonipara <yonisolo12@gmail.com>
2024-03-07 13:16:24 +0300

    python classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+    def __init__(self, size):
+        """Initialize an instance attribute size"""
+        self.__size = size

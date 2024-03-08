36ccd75cbc7c107f54196000ed7543c9a75adf13
yonipara <yonisolo12@gmail.com>
2024-03-08 14:13:29 +0300

    python classes

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,7 +5,12 @@
 class Square:
     """Represent a square."""
     def __init__(self, size=0, position=(0, 0)):
-        """Initialize an instance attribute size"""
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+            position (int, int): The position of the new square.
+        """
         self.size = size
         self.position = (position[0], position[1])
 

7ebef7e5d4805b308bdcc81ec56dca4774f514c1
yonipara <yonisolo12@gmail.com>
2024-03-08 19:51:13 +0300

    python classes

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -50,7 +50,6 @@ class Square:
         """Print the square with the # character."""
         if self.__size == 0:
             print("")
-            return
 
         [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):

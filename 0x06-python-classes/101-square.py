493bb498d731927a6570e1d4d318e144804050c1
yonipara <yonisolo12@gmail.com>
2024-03-08 20:03:47 +0300

    python classes

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """Define a class Square."""
 
-
 class Square:
     """Represent a square."""
 
+    
     def __init__(self, size=0, position=(0, 0)):
         """Initialize a new square.
 
@@ -15,14 +15,10 @@ class Square:
         self.size = size
         self.position = position
 
-    def __str__(self):
-        return str(self.my_print())
-
-
     @property
     def size(self):
         """Get/set the current size of the square."""
-        return self.__size
+        return (self.__size)
 
     @size.setter
     def size(self, value):
@@ -35,7 +31,7 @@ class Square:
     @property
     def position(self):
         """Get/set the current position of the square."""
-        return self.__position
+        return (self.__position)
 
     @position.setter
     def position(self, value):
@@ -48,7 +44,7 @@ class Square:
 
     def area(self):
         """Return the current area of the square."""
-        return self.__size * self.__size
+        return (self.__size * self.__size)
 
     def my_print(self):
         """Print the square with the # character."""
@@ -61,3 +57,14 @@ class Square:
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
             print("")
+
+    def __str__(self):
+        """Define the print() representation of a Square."""
+        if self.__size != 0:
+            [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            if i != self.__size - 1:
+                print("")
+        return ("")

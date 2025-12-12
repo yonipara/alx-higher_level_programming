dfb9125287a046cab78e50b03198e4fe1a728c56
yonipara <yonisolo12@gmail.com>
2025-12-12 10:16:58 -0600

    SQL database management

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- This script creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiple rows.
-CREATE TABLE second_table
+CREATE TABLE IF NOT EXISTS second_table 
 (
 	id	INT,
 	name	VARCHAR(256),

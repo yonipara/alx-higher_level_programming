89e43ded0bf1fadf13f92acad4dfe2d95843156b
yonipara <yonisolo12@gmail.com>
2025-12-12 11:11:28 -0600

    SQL database management

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- This script lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
+-- ordered by score (top first)
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;

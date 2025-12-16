0de561cfa69807b834e1f95abbdb15cefaec5d39
yonipara <yonisolo12@gmail.com>
2025-12-15 21:28:27 -0600

    SQL database management

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -5,7 +5,3 @@ ALTER DATABASE hbtn_0c_0
 ALTER TABLE first_table
   CONVERT TO CHARACTER SET utf8mb4
   COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table
-  MODIFY name VARCHAR(256)
-  CHARSET utf8mb4
-  COLLATE utf8mb4_unicode_ci;

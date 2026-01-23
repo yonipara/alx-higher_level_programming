748baaabd5a59f85c51daf495b33cb3723fd140e
yonipara <yonisolo12@gmail.com>
2026-01-23 13:49:17 -0600

    SQL database management

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+-- This script displays the average temperature (Fahrenheit) by city ordered by temperature (descending) during july and august.
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+WHERE month = 7 OR month = 8
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;

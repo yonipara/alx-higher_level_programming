8baf20a727217a51c57fb3b08150a4485b724304
yonipara <yonisolo12@gmail.com>
2026-02-05 16:52:15 -0600

    SQL - queries and joins

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- This script lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
+-- The SHOW GRANTS FOR 'user' syntax lists all the privileges for the specified user.
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
---
+++ 0x0E-SQL_more_queries/README.md
@@ -0,0 +1,1 @@
+These are all database management system problems using the SQL language

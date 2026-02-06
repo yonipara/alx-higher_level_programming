b543eac33ee12027393d313bbf2fb3f694706be2
yonipara <yonisolo12@gmail.com>
2026-02-05 22:13:34 -0600

    SQL - queries and joins

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
-/* This script creates the database hbtn_0d_usa and the table states 
-(in the database hbtn_0d_usa) on your MySQL server with the attribute
-id INT unique, auto generated, not null and is a primary key and
-name VARCHAR(256) not null */
+-- This script creates the database hbtn_0d_usa and the table states 
+-- (in the database hbtn_0d_usa) on your MySQL server with the attribute
+-- id INT unique, auto generated, not null and is a primary key and
+-- name VARCHAR(256) not null */
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states

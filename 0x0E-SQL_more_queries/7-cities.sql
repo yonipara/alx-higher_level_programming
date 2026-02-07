2dfb891e051c45b6e2ca6409b490689c6de6690f
yonipara <yonisolo12@gmail.com>
2026-02-06 18:37:59 -0600

    SQL - queries and joins

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,7 @@
-/* This script creates the database hbtn_0d_usa and the table cities
-(in the database hbtn_0d_usa) on your MySQL server with the attribute
-id INT unique, auto generated, not null and state_id which is a foreign 
-key that references to id of the states table and
-name VARCHAR(256) not null */
+-- This script creates the database hbtn_0d_usa and the table cities
+-- (in the database hbtn_0d_usa) on your MySQL server with the attribute
+-- id INT unique, auto generated, not null and state_id which is a foreign 
+-- key that references to id of the states table and
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities

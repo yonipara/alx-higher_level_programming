3552fe798a14920bc5812fe246983713c4729f56
yonipara <yonisolo12@gmail.com>
2026-02-06 18:34:01 -0600

    SQL - queries and joins

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,15 @@
+/* This script creates the database hbtn_0d_usa and the table cities
+(in the database hbtn_0d_usa) on your MySQL server with the attribute
+id INT unique, auto generated, not null and state_id which is a foreign 
+key that references to id of the states table and
+name VARCHAR(256) not null */
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities
+(
+	id INT NOT NULL AUTO_INCREMENT,
+        state_id INT NOT NULL, 	
+	name VARCHAR(256) NOT NULL,
+        PRIMARY KEY (id),
+	FOREIGN KEY(state_id) REFERENCES states(id)
+);

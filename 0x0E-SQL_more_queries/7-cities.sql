dab6d6353ee55e80991ca237e94540b193d31618
yonipara <yonisolo12@gmail.com>
2026-02-06 18:40:40 -0600

    SQL - queries and joins

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -7,7 +7,7 @@ USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities
 (
 	id INT NOT NULL AUTO_INCREMENT,
-        state_id INT NOT NULL, 	
+        state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
         PRIMARY KEY (id),
 	FOREIGN KEY(state_id) REFERENCES states(id)

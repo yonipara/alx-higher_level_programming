d6ebaf59537b8dbd80920cb9faade48055eb665e
yonipara <yonisolo12@gmail.com>
2026-02-10 18:22:30 -0600

    SQL - queries and joins

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,7 @@
+-- This script lists all shows contained in hbtn_0d_tvshows that have a genre or no genre at all.
+-- Each record displays: tv_shows.title - tv_show_genres.genre_id.
+-- Results are sorted in ascending order by tv_shows.title and tv_show_genres.genre_id.
+SELECT title, genre_id
+FROM tv_shows LEFT OUTER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY title, genre_id;

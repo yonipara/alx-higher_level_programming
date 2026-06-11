ab2ba14958ffe35d2e9cd1b827b60ba966ef7039
yonipara <yonisolo12@gmail.com>
2026-06-11 02:09:02 -0500

    java script warmup

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 function factorial (a) {
   if ((a - 0) && (a > 1)) {
-    var num = a * factorial(a - 1);
+    a = a * factorial(a - 1);
   } else {
     return 1;
   }
-  return num;
+  return a;
 }
 console.log(factorial(process.argv[2]));

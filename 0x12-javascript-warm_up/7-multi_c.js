d5acf5778752ead9c5792b8eb39df461a3c0a35d
yonipara <yonisolo12@gmail.com>
2026-06-10 22:43:47 -0500

    java script warmup

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+if (process.argv[2] - 0) {
+  let i = 0;
+  while (i < process.argv[2]) {
+    console.log('C is fun');
+    i++;
+  }
+} else {
+  console.log('Missing number of occurrences');
+}

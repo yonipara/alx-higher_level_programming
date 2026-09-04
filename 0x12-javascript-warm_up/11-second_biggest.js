3f107fd3eb67d3e1041aff4417f9bc824ec5a583
yonipara <yonisolo12@gmail.com>
2026-09-04 10:56:08 -0500

    java script warmup

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,26 +2,25 @@
 let arr = [];
 let temp;
 let newTemp;
-if (process.argv.length <= 2) {
+if (process.argv.length <= 3) {
 	console.log(0);
 }
-for (let i = 0; i < process.argv.length - 2; i++) {
-	arr[i] = process.argv[2 + i] -0;
-}
-console.log(arr);
-temp = (arr[0] > arr[1]) ? arr[0] : arr[1];
-for (let i = 2; i < arr.length; i++) {
-	if (temp < arr[i]) {
-		temp = arr[i];
+else {
+	for (let i = 0; i < process.argv.length - 2; i++) {
+		arr[i] = process.argv[2 + i] -0;
 	}
+	temp = (arr[0] > arr[1]) ? arr[0] : arr[1];
+	for (let i = 2; i < arr.length; i++) {
+		if (temp < arr[i]) {
+			temp = arr[i];
+		}
+	}
+	let newArr = arr.filter((n) => n != temp);
+	newTemp = (newArr[0] > newArr[1]) ? newArr[0] : newArr[1];
+	for (let i = 2; i < newArr.length; i++) {
+        	if (newTemp < newArr[i]) {
+                	newTemp = newArr[i];
+        	}
+	}
+	console.log(newTemp);
 }
-console.log(temp);
-let newArr = arr.filter((n) => n != temp);
-console.log(newArr);
-newTemp = (newArr[0] > newArr[1]) ? newArr[0] : newArr[1];
-for (let i = 2; i < newArr.length; i++) {
-        if (newTemp < newArr[i]) {
-                newTemp = newArr[i];
-        }
-}
-console.log(newTemp);

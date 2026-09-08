75a85cc4b779f0cae721f557d0f445e7b5c6aa58
yonipara <yonisolo12@gmail.com>
2026-09-08 11:08:00 -0500

    java script warmup

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,25 +1,37 @@
 #!/usr/bin/node
-const arr = [];
-let temp;
-let newTemp;
-if (process.argv.length <= 3) {
+const nums = process.argv.slice(2).map(Number);
+
+if (nums.length < 2) {
   console.log(0);
-} else {
-  for (let i = 0; i < process.argv.length - 2; i++) {
-    arr[i] = process.argv[2 + i] - 0;
-  }
-  temp = arr[0];
-  for (let i = 0; i < arr.length; i++) {
-    if (temp <= arr[i]) {
-      temp = arr[i];
-    }
-  }
-  const newArr = arr.filter((n) => n !== temp);
-  newTemp = newArr[0];
-  for (let i = 0; i < newArr.length; i++) {
-    if (newTemp <= newArr[i]) {
-      newTemp = newArr[i];
-    }
-  }
-  console.log(newTemp);
+  process.exit(0);
 }
+
+const max = Math.max(...nums);
+const secondMax = Math.max(...nums.filter(n => n !== max));
+
+console.log(secondMax);
+
+// const arr = [];
+// let temp;
+// let newTemp;
+// if (process.argv.length <= 3) {
+//  console.log(0);
+// } else {
+//  for (let i = 0; i < process.argv.length - 2; i++) {
+//    arr[i] = process.argv[2 + i] - 0;
+//  }
+//  temp = arr[0];
+//  for (let i = 0; i < arr.length; i++) {
+//    if (temp <= arr[i]) {
+//      temp = arr[i];
+//    }
+//  }
+//  const newArr = arr.filter((n) => n !== temp);
+//  newTemp = newArr[0];
+//  for (let i = 0; i < newArr.length; i++) {
+//    if (newTemp <= newArr[i]) {
+//      newTemp = newArr[i];
+//    }
+//  }
+//  console.log(newTemp);
+// }
